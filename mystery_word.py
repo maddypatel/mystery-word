2174e17871354ca18f2d5fab3b6a2c8dca1af443
Manish Patel <maddypatel@gmail.com>
2015-05-07 22:13:36 -0400

    Added documentation

--- mystery_word.py
+++ mystery_word.py
@@ -1,9 +1,19 @@
 import random
 
-def user_input():
+def game_mode():
+    """Returns game mode from the user input."""
     print ("Let's play \"Mystery Word\". You have 8 guesses to solve the word.")
     mode = input('\nChoose the game mode ("Easy, Normal or Hard"): ').lower()
+    return mode
+
+
+def list_of_words():
+    """Calls read_file to get a list of words.
+       Depending on the game mode, gets appropriate list.
+       From the list gets mystery word calling random_word.
+       Calls game with the mystery word argument."""
     words_list = read_file("/usr/share/dict/words")
+    mode = game_mode()
 
     if mode == "easy":
         words_list = easy_words(words_list)
@@ -16,6 +26,7 @@ def user_input():
     game(mystery)
 
 def random_word(words_list):
+    """Chooses random word from the list and returns"""
     mystery = random.choice(words_list)
     return mystery
 
@@ -48,9 +59,10 @@ def game(mystery):
 
     play_again = input("Play again? (Y or N): ").lower()
     if play_again == "y":
-        user_input()
+        list_of_words()
 
 def display_word(mystery, guesses):
+    """ Displays a string of guesses and '_' depending on guesses"""
     user_guess = ""
     for char in mystery:
         if char in guesses:
@@ -61,6 +73,7 @@ def display_word(mystery, guesses):
     return user_guess
 
 def is_word_complete(mystery, guesses):
+    """Checks to see if the word is complete"""
     missed = 0
     for char in mystery:
         if char not in guesses:
@@ -69,12 +82,14 @@ def is_word_complete(mystery, guesses):
         return True
 
 def read_file(input_file):
+    """Reads input_file and returns words in a list"""
     with open(input_file) as file:
         lines = file.read().upper()
     lines = lines.split()
     return lines
 
 def easy_words(my_list):
+    """Returns list of words that are 4 - 6 characters long"""
     easy_words_list = []
     for word in my_list:
         if len(word) >= 4 and len(word) <= 6:
@@ -82,6 +97,7 @@ def easy_words(my_list):
     return easy_words_list
 
 def medium_words(my_list):
+    """Returns list of words that are 6 - 8 characters long"""
     medium_words_list = []
     for word in my_list:
         if len(word) >= 6 and len(word) <= 8:
@@ -89,6 +105,7 @@ def medium_words(my_list):
     return medium_words_list
 
 def hard_words(my_list):
+    """Returns list of words that are 8 or more characters"""
     hard_words_list = []
     for word in my_list:
         if len(word) >= 8:
@@ -96,4 +113,4 @@ def hard_words(my_list):
     return hard_words_list
 
 if __name__ == "__main__":
-    user_input()
+    list_of_words()
